8b8898089f8253a3ca082d7f3fe5c6fdbfdbb012
Michelle <michelle.douma@gmail.com>
2022-09-29 11:21:59 -0300

    finsihing up client files

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -17,14 +17,57 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
-    ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+          }
+        }
       ],
-    },
+  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src/src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: "Text Editor Application",
+        short_name: "Text Edit",
+        description: "Text Editor!",
+        fingerprints: false,
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+            {
+                src: path.resolve("./src/images/logo.png"),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join("assets", "icons"),
+            },
+        ],
+      }),
+    ],
+  }
   };
-};
+
